47989bcf2ad579fdc9e5d79fd5e087e6d3cf78c6
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-10-18 11:49:10 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
 const h = require('react-hyperscript')
+const Header = require('./components/Header.js')
+const List = require('./components/List.js')
+const Form = require('./components/Form.js')
+var items = [{ id: 1,
+    title: 'JavaScript Fatigue',
+    link: 'https://hackernoon.com/how-it-feels-to-learn-javascript-in-2016-d3a717dd577f#.qtciu77bd',
+    score: 0
+  }]
 
-module.exports = () => h('div', 'Hello Mars')
+module.exports = () => h('div.pa4.bg-gray.vh-100', [
+  Header(),
+  h('main.avenir', [
+    List(items),
+    Form()
+  ])
+])
---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+const h = require('react-hyperscript')
+
+module.exports = _ => h('section.form', [
+  h('h2.f4', 'Add News'),
+  h('form', [
+    h('div.pb2', [
+      h('label.mr1', 'Title'),
+      h('input')
+    ]),
+    h('div.pb2', [
+      h('label.mr1', 'Link'),
+      h('input')
+    ]),
+    h('div.tr', [
+      h('button', 'Add News')
+    ])
+  ])
+
+])
